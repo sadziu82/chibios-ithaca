7dccdfa67e0ae2c5c8a930cfa5ebf188b65d84bf
Paweł Sadowski <pawel.sadowski@ithaca.pl>
2013-09-17 08:44:05 +0200

    added support for my own boards

---
+++ boards/copter_lqfp64_v1/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = $(BASEDIR)/chibios/ithaca/boards/copter_lqfp64_v1/board.c
+
+# Required include directories
+BOARDINC = $(BASEDIR)/chibios/ithaca/boards/copter_lqfp64_v1/
---
+++ boards/home_node_v1/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = $(BASEDIR)/chibios/ithaca/boards/home_node_v1/board.c
+
+# Required include directories
+BOARDINC = $(BASEDIR)/chibios/ithaca/boards/home_node_v1/
