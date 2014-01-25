6b93067180720f6b08c69a4e0c6696f5404300fb
Paweł Sadowski <pawel.sadowski@ithaca.pl>
2014-01-25 17:46:26 +0100

    Radio library prototype.

--- include/radio.h
+++ include/radio.h
@@ -1,53 +1,7 @@
-#ifndef _ITHACA_H_
-#define _ITHACA_H_
+#ifndef _RADIO_H_
+#define _RADIO_H_
 
-
-#include "ch.h"
-#include "hal.h"
-
-#include "ithacaconf.h"
-
-#if ITHACA_USE_LIB || defined(__DOXYGEN__)
-
-/*
- * @brief   ...
- * @details ...
- */
-typedef struct {
-    // ...
-    char *name;
-    bool flag;
-} lld_lock_t;
-
-#include <misc/device_id.h>
-
-#include <misc/console.h>
-
-#include <misc/block.h>
-#include <misc/rung.h>
-#include <misc/ladder.h>
-
-#include <misc/button.h>
-#include <misc/digital_output.h>
-#include <misc/hcsr501.h>
-#include <misc/keypad44.h>
-#include <misc/mono_timer.h>
-
-#include <misc/pca9633.h>
-#include <misc/lm75ad.h>
-#include <misc/dht11.h>
-
-#include <misc/font.h>
-#include <misc/font_std.h>
-#include <misc/lcd_st7735.h>
-
-#include <misc/imu.h>
-
-#include <radio.h>
-#include <radio/packet.h>
-#include <radio/rfm12b.h>
-#include <radio/mesh.h>
-#include <radio/rc.h>
+#if ITHACA_USE_RADIO || defined(__DOXYGEN__)
 
 /*===========================================================================*/
 /* Driver constants.                                                         */
@@ -76,19 +30,12 @@ typedef struct {
 #ifdef __cplusplus
 extern "C" {
 #endif
-//
-bool lldLock(lld_lock_t *lock);
-bool lldLockWaitTimeout(lld_lock_t *lock, systime_t tm);
-void lldUnlock(lld_lock_t *lock);
-bool lldLockISR(lld_lock_t *lock);
-void lldUnlockISR(lld_lock_t *lock);
-//
-extern EXTConfig EXTCFG1;
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* ITHACA_USE_LIB */
+#endif /* ITHACA_USE_RADIO */
 
-#endif /* _ITHACA_H_ */
+#endif /* _RADIO_H_ */
 
--- ithaca.mk
+++ ithaca.mk
@@ -21,44 +21,10 @@ CSRC += $(ITHACA)/src/misc/device_id.c \
         $(ITHACA)/src/misc/font_std.c \
         $(ITHACA)/src/misc/lcd_st7735.c \
         $(ITHACA)/src/misc/imu.c \
+        $(ITHACA)/src/radio/radio.c \
         $(ITHACA)/src/radio/packet.c \
         $(ITHACA)/src/radio/rfm12b.c \
         $(ITHACA)/src/radio/mesh.c \
         $(ITHACA)/src/radio/rc.c \
         $(ITHACA)/src/ithaca.c
-
-### ## misc
-### INCDIR += $(ITHACA)/include/
-### CSRC += $(ITHACA)/src/misc/chsprintf.c
-### CPPSRC += $(ITHACA)/src/misc/console.cpp
-### 
-### ## radio support
-### ifeq ($(ITHACA_RADIO),yes)
-### INCDIR += $(ITHACA)/include/radio/
-### CPPSRC += $(ITHACA)/src/radio/radio.cpp
-### ## rfm12b support
-### ifeq ($(ITHACA_RADIO_RFM12B),yes)
-### CPPSRC += $(ITHACA)/src/radio/radio_rfm12b.cpp
-### endif
-### ## rfm22b support
-### ifeq ($(ITHACA_RADIO_RFM22B),yes)
-### CPPSRC += $(ITHACA)/src/radio/radio_rfm22b.cpp
-### endif
-### endif
-### 
-### ifeq ($(ITHACA_FONT),yes)
-### ## font definitions
-### INCDIR += $(ITHACA)/include/font/
-### CPPSRC += $(ITHACA)/src/font/font.cpp \
-###           $(ITHACA)/src/font/font_std.cpp
-### endif
-### 
-### ifeq ($(ITHACA_LCD),yes)
-### ## font definitions
-### ## lcd drivers
-### INCDIR += $(ITHACA)/include/lcd/
-### CPPSRC += $(ITHACA)/src/lcd/lcd.cpp # \
-###           $(ITHACA)/src/lcd/lcd_st7735r.cpp # \
-###           $(ITHACA)/src/lcd/lcd_fsmc.cpp
-### endif
 
